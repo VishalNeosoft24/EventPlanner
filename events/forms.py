8a8c4776347ab794812ea5cca9d80f0bca88afe9
VishalNeosoft24 <vneosoft@gmail.com>
2025-04-15 23:42:22 +0530

    upadate form for bootstrap class

--- events/forms.py
+++ events/forms.py
@@ -10,7 +10,27 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['title', 'description', 'date', 'location', 'image']
+
+    def __init__(self, *args, **kwargs):
+        super(EventForm, self).__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Enter event title',
+            'description': 'Describe the event...',
+            'date': 'YYYY-MM-DD HH:MM',
+            'location': 'Where is the event?',
+            'image': '',
+        }
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = placeholders.get(field_name, '')
+
