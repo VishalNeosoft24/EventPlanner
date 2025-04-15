a914ab2259a5f153350eaccc5287ffbefc6fd282
VishalNeosoft24 <vneosoft@gmail.com>
2025-04-15 23:42:38 +0530

    change file path

--- events/models.py
+++ events/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+
+def event_image_upload_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"banner_{uuid.uuid4()}.{ext}"
+    return f'events/{instance.id or "new"}/{filename}'
+
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateTimeField()
     location = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='event_images/', null=True, blank=True)
+    image = models.ImageField(upload_to=event_image_upload_path, null=True, blank=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
