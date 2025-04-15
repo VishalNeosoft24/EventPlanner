4361b9b717c0ea9f148a51ae075eaa697445690b
Ubuntu <ubuntu@ip-172-31-3-42.ap-south-1.compute.internal>
2025-04-15 18:36:53 +0000

    Resolved merge conflict in settings.py and finalized AWS S3 & static settings

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-sx0ex(d#)3evbyvv==v=frcvpdxnz@*c=r&h4k(58*e@bp)qup
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
