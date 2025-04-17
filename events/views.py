d3ccffb092c4d761c6428b4c0e6f4bbc64cff7d3
VishalNeosoft24 <vneosoft@gmail.com>
2025-04-17 16:10:55 +0530

    practice merge conflict

--- events/views.py
+++ events/views.py
@@ -50,7 +50,7 @@ def login_view(request):
     else:
         form = AuthenticationForm()
 
-    # Add Bootstrap class dynamically
+    # Add Bootstrap class dynamically(boostrap5)
     for field in form.fields.values():
         field.widget.attrs.update({
             'class': 'form-control',
